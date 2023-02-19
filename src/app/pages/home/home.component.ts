001ad74545ef8913a26caafafe2aea06047bf4c6
ishakcoban <ishakcoban99@outlook.com>
2023-02-19 19:10:08 +0300

    loading indicator inserted to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  isLoading:boolean = true
+
+  ngAfterViewInit(){
+
+    setTimeout(()=>{
+      this.isLoading= false;
+    },2000)
+  }
 
 }
